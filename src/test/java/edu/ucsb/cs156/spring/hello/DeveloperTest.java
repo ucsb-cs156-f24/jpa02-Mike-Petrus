ab3b5a375fb50aee21f4adcdf5947ac7762bf546
Mike Petrus <Petrus@ucsb.edu>
2024-10-09 11:32:08 -0700

    mp - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -28,4 +28,9 @@ public class DeveloperTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+
+    @Test
+    public void geGithubId_returns_correct_githubId() {
+        assertEquals("Mike-Petrus", Developer.getGithubId());
+    }
 }
