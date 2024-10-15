c12f02b4c37075842cb720f616c2cc19cb2b7553
Mike Petrus <Petrus@ucsb.edu>
2024-10-14 22:43:27 -0700

    Tests done, mutations covered

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -11,7 +11,7 @@ public class TeamTest {
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");   
     }
 
     @Test
@@ -19,7 +19,43 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
+   @Test
+   public void toString_returns_correct_string() {
+    assertEquals("Team(name=test-team, members=[])", team.toString());
+   }
+
+   @Test
+   public void equals_returns_correct_case_1_and_2() {
+    assertEquals(true, team.equals(team));
+    assertEquals(false, team.equals(team.getMembers()));
+   }
+
+   @Test
+   public void equals_returns_correct_case_3() {
+    Team t1 = new Team("test-team");
+    assertEquals(true, team.equals(t1));
+
+    t1.setName("wrong");
+    assertEquals(false, team.equals(t1));
+
+    t1.setName("test-team");
+    t1.addMember("wrong");
+    assertEquals(false, team.equals(t1));
+   }
+
+   @Test
+   public void test_hash_code() {
+    Team t1 = new Team();
+    t1.setName("foo");
+    t1.addMember("bar");
+
+    int result = t1.hashCode();
+    int expectedResult = 130294;
+    assertEquals(expectedResult, result);
+   }
+
+
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
